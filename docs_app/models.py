3b5265e734b619e60641f2530533f7d53fc86330
timama <timforworking@mail.ru>
2024-04-03 16:26:12 +0300

    push

--- docs_app/models.py
+++ docs_app/models.py
@@ -12,7 +12,7 @@ class DamageImage(models.Model):
     extension = models.CharField(max_length=10, verbose_name='Расширение')
 
     def __str__(self):
-        return f'{self.original_name} ({self.event})'
+        return f'{self.original_name} ({self.damage})'
 
     class Meta:
         verbose_name = 'Фотография повреждения'

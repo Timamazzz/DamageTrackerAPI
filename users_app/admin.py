9fdff47e3632d9e93c276bba29c6b5d5a7a562dc
timama <timforworking@mail.ru>
2024-04-03 17:14:17 +0300

    push

--- users_app/admin.py
+++ users_app/admin.py
@@ -5,9 +5,32 @@ from django import forms
 
 
 class CustomUserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
     class Meta:
         model = User
-        fields = ('phone_number', 'password', 'is_employee')
+        fields = ['phone_number', 'password1', 'password2', 'is_employee']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['phone_number'].label = 'Phone Number'
+        self.fields['phone_number'].required = True
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get("password1")
+        password2 = cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
 
 
 @admin.register(User)

745d1a4ff130637aa7b7673a3c75c3e9ea0c4319
timama <timforworking@mail.ru>
2024-04-08 01:55:43 +0300

    push

--- acts_app/serializers/damage_serializers.py
+++ acts_app/serializers/damage_serializers.py
@@ -12,7 +12,7 @@ class DamageSerializer(WritableNestedModelSerializer):
 
 
 class DamageCreateSerializer(WritableNestedModelSerializer):
-    damage_images = DamageImageSerializer()
+    damage_images = DamageImageSerializer(required=False)
 
     class Meta:
         model = Damage

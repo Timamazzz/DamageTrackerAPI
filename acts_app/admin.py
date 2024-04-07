7889a5506743cc4a79dfdcc70e50a796f1c201f1
timama <timforworking@mail.ru>
2024-04-07 19:11:34 +0300

    psuh

--- acts_app/admin.py
+++ acts_app/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Municipality, BuildingType, Act, DamageType, DamageName, Damage
 
-# Register your models here.
+
+@admin.register(Municipality)
+class MunicipalityAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(BuildingType)
+class BuildingTypeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'is_victim']
+
+
+@admin.register(Act)
+class ActAdmin(admin.ModelAdmin):
+    list_display = ['number', 'created_at', 'name', 'employee', 'victim', 'municipality', 'address', 'building_type']
+
+
+@admin.register(DamageType)
+class DamageTypeAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(DamageName)
+class DamageNameAdmin(admin.ModelAdmin):
+    list_display = ['name', 'type']
+
+
+@admin.register(Damage)
+class DamageAdmin(admin.ModelAdmin):
+    list_display = ['act', 'damage_type', 'name', 'count', 'note']

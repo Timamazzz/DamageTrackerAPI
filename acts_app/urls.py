e88d1a00042774c9c56c7c00fbc1ccaa4b1cd709
timama <timforworking@mail.ru>
2024-04-12 16:38:46 +0300

    delete names

--- acts_app/urls.py
+++ acts_app/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from acts_app.views import ActViewSet, MunicipalityViewSet, BuildingTypeViewSet, DamageTypeViewSet, DamageNameViewSet
+from acts_app.views import ActViewSet, MunicipalityViewSet, BuildingTypeViewSet, DamageTypeViewSet
 
 router = DefaultRouter()
 
 router.register(r'municipalities', MunicipalityViewSet)
 router.register(r'building-types', BuildingTypeViewSet)
 router.register(r'damage-types', DamageTypeViewSet)
-router.register(r'damage-names', DamageNameViewSet)
 router.register(r'', ActViewSet)
 
 urlpatterns = [

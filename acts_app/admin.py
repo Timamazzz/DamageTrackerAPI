af5b277cd1cb6b3165d65ff621bc31c2b1c08188
Артем <timforworking@mail.ru>
2024-05-18 16:25:14 +0300

    push

--- acts_app/admin.py
+++ acts_app/admin.py
@@ -24,7 +24,7 @@ class AddressAdmin(admin.ModelAdmin):
 @admin.register(Act)
 class ActAdmin(admin.ModelAdmin):
     list_display = ['number', 'created_at', 'employee', 'victim', 'municipality', 'address', 'building_type']
-    list_filter = [('created_at', DateRangeFilter)]
+    list_filter = ['created_at', ('created_at', DateRangeFilter)]
 
     actions = ['export_acts_to_excel']
 

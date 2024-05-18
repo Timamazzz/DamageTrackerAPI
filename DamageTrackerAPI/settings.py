4680d20e6a7f954db4c13f1987a4dc77a4406566
Артем <timforworking@mail.ru>
2024-05-18 17:25:54 +0300

    push

--- DamageTrackerAPI/settings.py
+++ DamageTrackerAPI/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'jazzmin',
     'django.contrib.admin',
     'rest_framework',
     'corsheaders',

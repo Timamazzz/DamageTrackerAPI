63e6e416ad5c3c9d3954dfb6d827f4d916e57d0d
Артем <timforworking@mail.ru>
2024-05-18 15:52:57 +0300

    push

--- acts_app/admin.py
+++ acts_app/admin.py
@@ -21,6 +21,28 @@ class AddressAdmin(admin.ModelAdmin):
 class ActAdmin(admin.ModelAdmin):
     list_display = ['number', 'created_at', 'employee', 'victim', 'municipality', 'address', 'building_type']
 
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+
+        if request.user.is_staff:
+            if not request.user.municipality:
+                self.message_user(request, "У вас не указан муниципалитет.", level='error')
+                return qs.none()
+
+            user_municipality = request.user.municipality
+            filtered_qs = qs.filter(municipality=user_municipality) | qs.filter(employee=request.user)
+
+            if not filtered_qs.exists():
+                self.message_user(request, "Актов нет.", level='info')
+                return qs.none()
+
+            return filtered_qs
+
+        self.message_user(request, "У вас нет доступа к актам.", level='error')
+        return qs.none()
+
 
 @admin.register(DamageType)
 class DamageTypeAdmin(admin.ModelAdmin):
